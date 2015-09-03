7c793c6f1904659b30b3053b6682f48fb2841ec3
Seth Phillips <seth.phillips@trms.com>
2015-09-03 09:31:37 -0500

    change play button and hide cursor

--- resources/views/buttons.blade.php
+++ resources/views/buttons.blade.php
@@ -6,7 +6,7 @@
 		<div class="button-container">
 			@foreach ($buttons as $key=>$button)
 				<div class="button" data-target="{{ $button->id }}" data-url="{!! route('triggerVideo') !!}" @if($key !== 0) style="margin-left:{{ $marginLeft }}%" @endif>
-					<img src="{!! asset('img/play_button.png') !!}" class="button-img">
+					<img src="{!! asset('img/play_button_alt.png') !!}" class="button-img">
 					<p class="button-title">{{ $button->title }}</p>
 				</div>
 			@endforeach
