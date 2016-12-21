87f243a1ae5757cdbc33c92e227076066341de87
Seth Phillips <seth.phillips@trms.com>
2016-12-21 13:49:13 -0600

    make screen fillable

--- app/Button.php
+++ app/Button.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Button extends Model
 {
-    protected $fillable = ['title','tag','length','order'];
+    protected $fillable = ['title','tag','length','order','screen'];
 }
